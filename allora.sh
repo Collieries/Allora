997b41ddd8a992cca7d46cfac3b9c94718c207bd
Defjnite <98644935+Collieries@users.noreply.github.com>
2024-07-24 17:54:26 +0500

    Update allora.sh

--- allora.sh
+++ allora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\nПодписаться на канал - https://t.me/web3easy\n"
+echo -e "\nПодписаться на канал Web3easy\n"
 
 sleep 2
 
@@ -348,10 +348,11 @@ EOL
                 echo "Проверка статуса ноды: Ошибка"
             fi
 
-            echo -e "\nПодписаться на канал - https://t.me/web3easy\n"
+            echo -e "\nПодписаться на канал Web3easy\n"
             ;;
         2)
-            sleep 2
+            echo "Через 30 секунд пойдут логи. Для выхода из отображения логов нажмите CTRL+C\n"
+            sleep 60
             container_id=$(docker ps --filter "ancestor=basic-coin-prediction-node-worker" --format "{{.ID}}")
             if [ -z "$container_id" ]; then
                 echo "Контейнер с IMAGE 'basic-coin-prediction-node-worker' не найден."
